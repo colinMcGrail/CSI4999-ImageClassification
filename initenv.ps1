5a760a864096f8686e9047bc3c061803c882b17a
colinMcGrail <102310855+colinMcGrail@users.noreply.github.com>
2024-07-31 14:30:30 -0400

    Update initenv.ps1

--- initenv.ps1
+++ initenv.ps1
@@ -1,3 +1,3 @@
 python3.11 -m venv environment
 environment\Scripts\Activate.ps1
-python3.11 -m pip install numpy==1.23.5 streamlit==1.22.0 Pillow==9.5.0 keras==2.12.0 tensorflow==2.12.0
+pip install numpy==1.23.5 streamlit==1.22.0 Pillow==9.5.0 keras==2.12.0 tensorflow==2.12.0
