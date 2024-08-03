abe7631116c5771b5c4b45a2131246498221013b
colinMcGrail <102310855+colinMcGrail@users.noreply.github.com>
2024-08-03 17:16:57 -0400

    Update initenv.ps1

--- initenv.ps1
+++ initenv.ps1
@@ -1,4 +1,4 @@
 python3.11 -m venv environment
 environment\Scripts\Activate.ps1
-pip install streamlit Pillow tensorflow-cpu~=2.17.0 torch==2.4.0+cu121 torchvision==0.19.0+cu121 jax[cpu] --extra-index-url https://download.pytorch.org/whl/cu121
+pip install --upgrade streamlit Pillow tensorflow-cpu~=2.17.0 torch==2.4.0+cu121 torchvision==0.19.0+cu121 jax[cpu] --extra-index-url https://download.pytorch.org/whl/cu121
 pip install --upgrade keras
