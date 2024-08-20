41711f6de66188ff2f5e285d23657b416b33befc
shrutinp <shrutipurandare812@gmail.com>
2024-08-20 18:38:27 -0400

    added some designs to SignUp

--- page/general/signup.py
+++ page/general/signup.py
@@ -1,7 +1,54 @@
 import streamlit as st
 import sqlite3
+# Custom CSS to hide the Streamlit "Deploy" button and "Menu" button
+custom_css = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+        body {
+            # background-color: #462762;
+            color: white;
+        }
+        .form-container {
+            background-color: #462762;  /* Set the background color for the form container */
+            padding: 20px;
+            border-radius: 10px;
+        }
+        .stApp {
+            # background-color: #462762;
+            # color: white;
+        }
+        st.title {
+            font-size: 40px;
+            text-align: center;
+            font-weight: bold;
+            # margin-top: -60px;
+        }
+        .stTextInput, .stFormSubmitButton {
+            font-size: 18px;
+        }
+        .stButton>button {
+            background-color: #462762;
+            color: #ffffff;
+            border-radius: 5px;
+            border: 1px solid transparent;
+        }
+        .stButton>button:hover {
+            background-color: #ffffff;
+            color: #000000;
+            border-radius: 5px;
+            border: 1px solid #462762;
+        }
+</style>
+"""
 
-st.title('Sign Up')
+# Inject the custom CSS
+st.markdown(custom_css, unsafe_allow_html=True)
+
+st.title('MedImage Classifier')
+
+st.subheader('Sign Up')
 
 con = sqlite3.connect("data.db")
 cur = con.cursor()
