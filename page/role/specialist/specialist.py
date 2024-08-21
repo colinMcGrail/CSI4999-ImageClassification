bd492ed63b5f767dd48696a8af3d819e04ff2dfa
shrutinp <shrutipurandare812@gmail.com>
2024-08-20 22:36:32 -0400

    added some CSS to Specialist Physician Patient

--- page/role/specialist/specialist.py
+++ page/role/specialist/specialist.py
@@ -1,3 +1,54 @@
 import streamlit as st
 
+custom_css = """
+    <style>
+    MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+        body {
+            # background-color: #462762;
+            color: white;
+        }
+        .form-container {
+            background-color: #462762;  /* Set the background color for the form container */
+            padding: 20px;
+            border-radius: 10px;
+            width: 100%;
+        }
+        .form-container2 {
+            background-color: #462762;  /* Set the background color for the form container */
+            padding: 1px;
+            border-radius: 0px;
+            width: 100%;
+        }
+        .stApp {
+            # background-color: #462762;
+            # color: white;
+        }
+        .title {
+            font-size: 40px;
+            text-align: center;
+            font-weight: bold;
+            margin-top: -60px;
+        }
+        .stTextInput, .stFormSubmitButton {
+            font-size: 18px;
+        }
+        .stButton>button {
+            background-color: #462762;
+            color: #ffffff;
+            border-radius: 5px;
+            border: 1px solid transparent;
+        }
+        .stButton>button:hover {
+            background-color: #ffffff;
+            color: #000000;
+            border-radius: 5px;
+            border: 1px solid #462762;
+        }
+</style>
+"""
+
+# Inject the custom CSS
+st.markdown(custom_css, unsafe_allow_html=True)
 st.title("This is the specialist home page.")
